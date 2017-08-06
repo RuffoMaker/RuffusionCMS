fae27e1fcf4e48900ede23824826157c982a35d4
RuffoMaker <rubenelinformatico@gmail.com>
2017-08-06 02:22:09 +0200

    Autohook

--- application/modules/autohook/controllers/autohook.php
+++ application/modules/autohook/controllers/autohook.php
@@ -10,12 +10,14 @@ class Autohook extends MX_Controller
 	{
 		if ( isset($_POST['payload']) && $_POST['payload'] ) {
 			echo 'Haciendo pull...';
+			echo "\n";
 			//print(chdir('/home2/zarabits/www/ruffoschool.com'));
   		if(exec( '/usr/bin/git pull origin master', $output)){
   			echo 'Correcto.';
   		}
   		else{
-  			echo 'Error: '.$output;
+  			echo 'Error: ';
+  			var_dump($output);
   		}
 		}
 		else{
